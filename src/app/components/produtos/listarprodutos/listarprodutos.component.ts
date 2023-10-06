aded915f179a5044b4c901f6f31700d9c5f68272
Manuela <Manuelafonsecaramos@gmail.com>
2023-10-05 22:27:56 -0300

    Aula 10 - Formulario (EM ANDAMENTO)

--- src/app/components/produtos/listarprodutos/listarprodutos.component.ts
+++ src/app/components/produtos/listarprodutos/listarprodutos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Console } from 'console';
 
 @Component({
   selector: 'app-listarprodutos',
@@ -7,7 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarprodutosComponent implements OnInit {
 
-  constructor() { }
+  listaStrings: string[] = ['Primeiro', 'Segundo', 'Terceiro'];
+  listaNumeros: number[] = [15,15,18, 100];
+
+  objetoModelo = {
+    nome: 'Manuela',
+    idade: 16,
+    altura: 1.65,
+    graduado: true
+  };
+  listaProdutos: any[] = [
+  {nome:'Curso de  Angular', precoProduto: 35.56, validade: '2021-12-31', id:1},
+  {nome: 'Curso de Ionic', precoProduto: 50, validade: '2021-12-31', id:2},
+  {id: 3, nome:'Curso de Ionic Avançado', precoProduto: 50, validade: '2021-12-31'},
+  ];
+
+  constructor() {
+    for (let item of this.listaStrings) {
+      console.log(item);
+    }
+    for(const item of this.listaNumeros){
+      console.log(item);
+    }
+    console.log(this.objetoModelo);
+    console.log(this.objetoModelo.nome);
+
+   }
 
   ngOnInit(): void {
   }
